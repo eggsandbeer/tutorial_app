826ce3db1c6c6c69bdcbfb97a87d63b5dab58af0
Cameron Strandberg <cameron.strandberg@gmail.com>
2012-12-19 16:02:33 -0500

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,10 @@ New::Application.routes.draw do
 
   get "pages/help"
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
+  match '/contact' => 'pages#contact'
+  match '/help' => 'pages#help'
+  match '/about' => 'pages#about'
+  
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
@@ -56,7 +55,7 @@ New::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
