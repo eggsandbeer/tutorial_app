d1f66dfbf67d88e482641056ff2bf56646257ea2
Cameron Strandberg <cameron.strandberg@gmail.com>
2012-12-28 10:00:59 -0500

    Done with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 New::Application.routes.draw do
+  get "sessions/new"
+
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout',  :to => 'sessions#destroy'
 
   match '/contact' => 'pages#contact'
   match '/help' => 'pages#help'
